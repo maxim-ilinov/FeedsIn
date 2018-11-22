12fae633b1902a1c4747be0bbfdaf9df929abd3c
Maxim Ilinov <maxim.ilinov@gmail.com>
2018-11-22 18:07:07 +0300

    init

---
+++ app/src/main/java/com/maxim_ilinov_gmail/feedsin/MainActivity.java
@@ -0,0 +1,21 @@
+package com.maxim_ilinov_gmail.feedsin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends SingleFragmentActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // fill rvRSSItems
+
+
+
+
+
+    }
+}
